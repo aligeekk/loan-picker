switch to new calculation of portfolio returns. revamp expected NAR calcs. Fix bug in grade-based model calc
Former-commit-id: 6cb76463a0f3284127d5f989e0a4b90b86d18dad [formerly ec60ed2948af5ddca907dc42f6c3ca84ea91a58b]
Former-commit-id: dc4c121cc3c186ad17c1a0be8e431e40092a0e2e

LC_model_analysis.py
@@ -25,8 +25,8 @@ import datetime
 
 plot_figures = True
 run_CV = False
-base_dir = os.path.dirname(os.path.realpath(__file__))
-#base_dir = '/Users/james/Data_Incubator/loan-picker'
+#base_dir = os.path.dirname(os.path.realpath(__file__))
+base_dir = '/Users/james/Data_Incubator/loan-picker'
     
 sys.path.append(base_dir)
 import LC_helpers as LCH
@@ -43,10 +43,6 @@ movie_dir = os.path.join(base_dir,'static/movies/')
 #load data 
 data_name = 'all_loans_proc'
 LD = pd.read_csv(data_dir + data_name, parse_dates=['issue_d',])
-
-#load long/lat data for each zip-code
-zip3_data = LCL.load_location_data(data_dir,group_by='zip3')        
-LD = pd.merge(LD,zip3_data,how='inner', left_on='zip3', right_index=True)
 
 #%%
 predictor = namedtuple('predictor', ['col_name', 'full_name', 'norm_type'])
@@ -94,11 +90,8 @@ predictors = [
 #%%
 response_var = 'ROI'
 y = LD[response_var].values  # response variable
-# weight_y = (LD['mnthly_ROI'] * LD['exp_num_pymnts']).values #duration-weighted returns
-# Npymnts = LD['exp_num_pymnts'].values # expected number of payments
-weight_y = LD['mnthly_ROI']
-Npymnts = np.ones_like(y) # expected number of payments
-    
+net_returns = LD['net_returns'].values
+prnc_weights = LD['prnc_weight'].values
 LD.fillna(0, inplace=True)
 
 print('Transforming data')
@@ -122,6 +115,7 @@ for idx, feature_name in enumerate(feature_names):
         X[:,idx] = tran_dict[use_cols[pidx]].fit_transform(X[:,idx].reshape(-1,1)).squeeze()
 
 dict_vect.tran_dict = tran_dict
+
 #%% COMPILE LIST OF MODELS TO COMPARE
     
 Lin_est = Ridge()
@@ -181,13 +175,14 @@ else:
 
 
 #%%
-model_set = [('Null',LCM.rand_pick_mod()),
-             ('Lin', Lin_model),
-             ('Lin_SVR',SVR_model),
-             ('GBR',GBR_model),
-             ('RF', RF_model)]
-#model_set = [('Lin', Lin_model),
+#model_set = [('Null',LCM.rand_pick_mod()),
+#             ('Lin', Lin_model),
+#             ('Lin_SVR',SVR_model),
+#             ('GBR',GBR_model),
 #             ('RF', RF_model)]
+model_set = [('Null',LCM.rand_pick_mod()),
+            ('Lin', Lin_model),
+             ('RF', RF_model)]
 
 #%%
 n_folds = 5
@@ -205,17 +200,17 @@ returns = defaultdict(list)
 marg_returns = []
 grade_returns = defaultdict(list)
 RF_feature_imp = defaultdict(list)
-grade_makeup = {name:np.zeros((len(kf),len(unique_grades))) for name,_ in model_set}
+grade_makeup = {name:np.zeros((len(kf),len(unique_grades))) for name, _ in model_set}
 cnt = 0
 np.random.seed(0)
 for train, test in kf:
  print('CV split {} of {}'.format(cnt, n_folds))
- marg_returns.append(np.mean(weight_y[test]) / np.mean(Npymnts[test]))
+ marg_returns.append(np.sum(net_returns[test]) / np.sum(prnc_weights[test]))
  for name, model in model_set:
      model.fit(X[train,:], y[train])
-     train_R2[name].append(model.score(X[train,:],y[train]))
+#     train_R2[name].append(model.score(X[train,:],y[train]))
      test_R2[name].append(model.score(X[test,:],y[test]))
-     train_pred = model.predict(X[train,:])
+#     train_pred = model.predict(X[train,:])
      test_pred = model.predict(X[test,:])
 
      if name == 'RF': #test feature importance for RF model
@@ -229,11 +224,14 @@ for train, test in kf:
                  shuff_R2[n] = model.score(Xshuff, y[test])    
              RF_feature_imp[col_name].append(test_R2[name][-1] - np.mean(shuff_R2))
   
-     returns[name].append(LCM.pick_K_returns_BTSTRP(weight_y[test], 
-                             test_pred, pick_K_list,Npymnts[test],n_boots=100, sub_marg=False))
-     grade_returns[name].append(LCM.pick_K_returns_by_grade(weight_y[test], 
-                             test_pred,LD.iloc[test][grade_group],grade_pick_K,
-                             Npymnts[test]))
+     returns[name].append(LCM.pick_K_returns_BTSTRP( 
+                             test_pred, net_returns[test], prnc_weights[test],
+                            pick_K_list, n_boots=100, sub_marg=False))
+                            
+     grade_returns[name].append(LCM.pick_K_returns_by_grade(
+                             test_pred, net_returns[test], prnc_weights[test],
+                            LD.iloc[test][grade_group], grade_pick_K))
+                            
      grade_makeup[name][cnt,:] = LCM.get_choice_grade_makeup(test_pred, LD.iloc[test][grade_group], 
                                                             grade_group, unique_grades, grade_pick_K)     
  cnt += 1
@@ -243,9 +241,11 @@ marg_returns = LCM.annualize_returns(np.array(marg_returns))
 for name, model in model_set:
     returns[name] = LCM.annualize_returns(np.array(returns[name]))
     rel_returns[name] = returns[name] - marg_returns[:,np.newaxis,np.newaxis]  
-    grade_returns[name] = LCM.annualize_returns(np.array(grade_returns[name]))
     returns[name] = np.reshape(returns[name], (-1,len(pick_K_list)))
     rel_returns[name] = np.reshape(rel_returns[name], (-1,len(pick_K_list)))
+    
+    grade_returns[name] = LCM.annualize_returns(np.array(grade_returns[name]))
+    grade_returns[name] = np.reshape(grade_returns[name], -1, len(unique_grades))
 
 
 #%% PLOT RELATIVE FEATURE IMPORTANCES FOR FULL RF MODEL
@@ -367,9 +367,10 @@ avg_Gint_rates = LD.groupby(grade_group)['int_rate'].mean()
 avg_Gint_rates.sort_index(inplace=True)
 best_returns = 100 * ((avg_Gint_rates/100/12 + 1) ** 12 - 1)
 
-mod_list = ['Null','Lin','Lin_SVR','GBR','RF']
+mod_list = [name for name,_ in model_set]
 n_mods = len(mod_list)
-mod_names = ['Random','Linear','Linear SVR','Gradient Boosting','Random Forest']
+#mod_names = ['Random','Linear','Linear SVR','Gradient Boosting','Random Forest']
+mod_names = ['Random','Linear','Random Forest']
 pal = sns.color_palette("muted", n_colors=len(mod_list))
 
 jitt_x = 0.6
@@ -382,12 +383,12 @@ for idx, mod_name in enumerate(mod_list):
     plt.errorbar(np.arange(len(grades)) + idx*jitt_x/n_mods - jitt_x/(2.),
                  np.mean(grade_returns[mod_name],axis=0),
                  np.std(grade_returns[mod_name],axis=0)/err_norm, 
-                color=pal[idx], label=mod_names[idx], lw=2, fmt='o-', ms=10, alpha=alpha)
+                color=pal[idx], label=mod_names[idx], lw=2, fmt='o', ms=10, alpha=alpha)
                  
 plt.plot(np.arange(len(grades)), best_returns,'k',lw=2, 
              ls='dashed', label='Interest rate')
              
-plt.xlim(-0.2, 5.2)
+plt.xlim(-0.4, 5.2)
 plt.ylim(0,25)
 grades = np.concatenate(([''],grades))
 ax.set_xticklabels(grades, fontsize=14)
@@ -412,7 +413,7 @@ n_trees=100
 min_samples_leaf=100
 
 #dont use time for this analysis
-use_cols = [col for col in all_pred_cnames if col != 'issue_day']
+use_cols = [col for col in use_cols if col != 'issue_day']
 RF_est = RandomForestRegressor(n_estimators=n_trees, max_depth=10, 
                                min_samples_leaf=min_samples_leaf, n_jobs=4)
 RF_mod = Pipeline([('col_sel',LCM.col_selector(use_cols,col_dict)),
@@ -439,17 +440,15 @@ for idx, test_pt in enumerate(test_pts):
     if n_test > 0:    
         stream_R2[idx] = RF_mod.score(X[test_set,:],y[test_set])
         test_pred = RF_mod.predict(X[test_set,:])
-        stream_returns[idx,:] = (LCM.pick_K_returns_BTSTRP(weight_y[test_set], 
-                                test_pred, pick_K_list,Npymnts[test_set],
+        stream_returns[idx,:] = (LCM.pick_K_returns_BTSTRP( 
+                                test_pred, net_returns[test_set], 
+                                prnc_weights[test_set],pick_K_list,
                                 n_boots=100, sub_marg=False)).squeeze()
                  
 stream_returns = LCM.annualize_returns((stream_returns))
 
 #%%
-# LD['weight_y'] = (LD['mnthly_ROI'] * LD['exp_num_pymnts'])
-# marg_returns = LD.groupby('issue_d')['weight_y'].mean()
-marg_returns = LD.groupby('issue_d')['mnthly_ROI'].mean()
-# marg_returns = marg_returns / LD.groupby('issue_d')['exp_num_pymnts'].mean()
+marg_returns = LD.groupby('issue_d')['net_returns'].sum() / LD.groupby('issue_d')['prnc_weight'].sum()
 marg_returns = LCM.annualize_returns(marg_returns)
 
 actual_dates = [LD['issue_d'].min() + datetime.timedelta(days=test_pt) for test_pt in test_pts]
@@ -464,15 +463,17 @@ fig,ax=plt.subplots()
 ax.plot(actual_dates[use_pts],stream_avg_returns[use_pts],'o-',
         color=pal[0],label='model-based selection')
 marg_returns.plot(label='marginal returns',color=pal[2])
+ax.set_ylim(0,25)
 ax2 = ax.twinx()
 date_counts = LD.groupby('issue_d')['issue_d'].count()  
 ax2.bar(date_counts.index.values,date_counts/1e3,width=10,fc='k',alpha=0.25) 
    
 ax.set_ylabel('Annualized returns (%)', color='k',fontsize=14)
-ax2.set_ylabel('Loan volume (thousands)', color='gray',fontsize=14)
+ax2.set_ylabel('Loan volume', color='gray',fontsize=14)
 ax.set_xlabel('Issue date',fontsize=14)
 ax.set_xlim(datetime.date(2009,01,01),LD['issue_d'].max())
 ax.legend(loc='upper left',fontsize=14)
+ax2.set_yticks([])
 
 ymin, ymax= ax.get_ylim()
 xmin, xmax = ax.get_xlim()
@@ -487,7 +488,7 @@ xmin = mdates.date2num(xmin)
 xmax = mdates.date2num(xmax)
 ax.add_patch(Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, alpha=0.25,
                        facecolor=pal[1]))
-ax.annotate('Training data', xy=((xmin+xmax)/2-50., ymax-2), xytext=(xmin-700, ymax-3),
+ax.annotate('Training data', xy=((xmin+xmax)/2-50., ymax-1.5), xytext=(xmin-700, ymax-2),
             arrowprops=dict(facecolor='black', shrink=0.05))
 
 if plot_figures:

LC_models.py
@@ -34,43 +34,35 @@ def pick_K_returns(y, y_pred, K_list, num_pymnts, sub_marg=True):
         pick_k_avgs = pick_k_avgs - marg_return
     return pick_k_avgs
 
-def pick_K_returns_BTSTRP(y, y_pred, K_list, num_pymnts, n_boots=500, sub_marg=True):
+def pick_K_returns_BTSTRP(y_pred, returns, weights, K_list, n_boots=500, sub_marg=True):
     '''metric based on the average of the data on the top K
     data points ranked by model predicted returns'''
-    if type(y) == pd.core.series.Series:
-        y_comb = pd.DataFrame({'obs': y.values, 'pred':y_pred,
-                                'num_pymnts': num_pymnts})
-    else:
-        y_comb = pd.DataFrame({'obs': y, 'pred':y_pred,
-                        'num_pymnts': num_pymnts})
+    y_comb = pd.DataFrame({'returns': returns, 'pred':y_pred,
+                        'weights': weights})
 
     pick_k_avgs = np.zeros((n_boots,len(K_list)))
     for n in xrange(n_boots):
-        y_samp = y_comb.sample(frac=1, replace=True)
-        y_samp = y_samp.sort_values(by='pred',ascending=False)
+        y_samp = y_comb.sample(frac=1, replace=True) #resample with replacement
+        y_samp = y_samp.sort_values(by='pred',ascending=False) #sort descending by predicted value
         for idx, K in enumerate(K_list):
-            picked_loans = y_samp.head(K)
-            pick_k_avgs[n,idx] = picked_loans['obs'].mean() / picked_loans['num_pymnts'].mean()
-        # pick_k_avgs[n,idx] = y_samp['obs'].head(K).mean()
+            picked_loans = y_samp.head(K) #take top K based on predicted
+            pick_k_avgs[n,idx] = picked_loans['returns'].sum() / picked_loans['weights'].sum()
         if sub_marg:
-            marg_returns = y_samp['obs'].mean() / y_samp['num_pymnts'].mean()
+            marg_returns = y_samp['returns'].sum() / y_samp['weights'].sum()
             pick_k_avgs[n,:] = pick_k_avgs[n,:] - marg_returns
     return pick_k_avgs
 
-def pick_K_returns_by_grade(y, y_pred, grades, K, num_pymnts):
+def pick_K_returns_by_grade(y_pred, returns, weights, grades, K, n_boots=100):
     '''metric based on the average of the data on the top K
     data points ranked by model predicted returns'''
-    if type(y) == pd.core.series.Series:
-        y_comb = pd.DataFrame({'obs': y.values, 'pred':y_pred, 
-                        'grade':grades, 'num_pymnts': num_pymnts})
-    else:
-        y_comb = pd.DataFrame({'obs': y, 'pred':y_pred, 
-                        'grade':grades, 'num_pymnts': num_pymnts})
-    y_comb = y_comb.sort_values(by='pred',ascending=False)
-    picked_loans = y_comb.groupby('grade').head(K).groupby('grade')
-    pick_k_avgs = picked_loans['obs'].mean() / picked_loans['num_pymnts'].mean()
-    pick_k_avgs = pick_k_avgs.sort_index() # sort by grade in alphabetical order  
-    # return pick_k_avgs - y_comb['obs'].mean()
+    y_comb = pd.DataFrame({'return': returns, 'pred':y_pred, 
+                        'grade':grades, 'weight': weights})
+    pick_k_avgs = np.zeros((n_boots, len(y_comb['grade'].unique())))
+    for n in xrange(n_boots):
+        y_samp = y_comb.sample(frac=1, replace=True) #resample with replacement
+        picked_loans = y_samp.groupby('grade').apply(lambda g: g.sort_values(by='pred', ascending=False).head(K))
+        picked_loans = picked_loans.groupby('grade')
+        pick_k_avgs[n,:] = picked_loans['return'].sum() / picked_loans['weight'].sum()
     return pick_k_avgs 
 
 def get_choice_grade_makeup(preds, grades, grade_group, unique_grades, K):

expected_returns.py
@@ -48,7 +48,7 @@ print_figs = False
 #load long/lat data for each zip-code
 zip3_data = LCL.load_location_data(data_dir,group_by='zip3') 
 LD['zip3'] = LD['zip3'].astype(int)       
-LD = pd.merge(LD,zip3_data,how='inner', left_on='zip3', right_index=True)
+LD = pd.merge(LD, zip3_data, how='inner', left_on='zip3', right_index=True)
 
 #%% Compute hazard functions for each loan grade and term 
 term_bandwidths = [4., 8.] #list of NAF smoothing bandwidth (for each term)
@@ -71,52 +71,26 @@ for idx,term in enumerate(keep_terms): #compute all hazard functions for each te
         
 #%%
 terms = LD.term.unique() #set of unique loan terms
-for term in terms: #for each possible loan term
-    
+for term in terms: #for each possible loan term  
     #get relevant set of loans
     cur_loans = LD.term == term 
     cur_LD = LD[cur_loans]
     
-    (NAR, weighted_NAR, MAR) = LCH.get_NARs(cur_LD, term)
-    LD.ix[cur_loans,'NAR'] = NAR #measured performance of each loan
-    LD.ix[cur_loans,'weighted_NAR'] = weighted_NAR #performance weighted by time contribution to imaginary portfolio
-    LD.ix[cur_loans,'MAR'] = MAR #principal weighted avg monthly returns
+    (NAR, net_returns, p_csum) = LCH.get_NARs(cur_LD, term)
+    LD.ix[cur_loans,'ROI'] = NAR #measured performance of each loan
+    LD.ix[cur_loans,'net_returns'] = net_returns #principal weighted avg monthly returns
+    LD.ix[cur_loans,'prnc_weight'] = p_csum #principal weighted avg monthly returns
+    LD.ix[cur_loans,'default_prob'] = LD.ix[cur_loans,'is_observed'].astype(float) #principal weighted avg monthly returns
 
-    (exp_NAR, weighted_exp_NAR, exp_MAR, tot_default_prob, exp_num_pymnts) = \
+    (exp_NAR, tot_default_prob, exp_num_pymnts, exp_net_returns, exp_csum) = \
             LCH.get_expected_NARs(cur_LD, term, all_hazards[term])
-    LD.ix[cur_loans,'exp_NAR'] = exp_NAR
-    LD.ix[cur_loans,'weighted_exp_NAR'] = weighted_exp_NAR
-    LD.ix[cur_loans,'exp_MAR'] = exp_MAR # expected monghtly avg returns
+    LD.ix[cur_loans,'ROI'] = exp_NAR
     LD.ix[cur_loans,'default_prob'] = tot_default_prob
     LD.ix[cur_loans,'exp_num_pymnts'] = exp_num_pymnts
-    
-#for observed loans use the NAR and for censored used the expected NAR
-LD['ROI'] = np.zeros((len(LD),))
-LD['weighted_ROI'] = np.zeros((len(LD),))
+    LD.ix[cur_loans,'net_returns'] = exp_net_returns
+    LD.ix[cur_loans,'prnc_weight'] = exp_csum   
 
 LD.ix[LD.is_observed,'exp_num_pymnts'] = LD.ix[LD.is_observed,'num_pymnts']
-
-LD.ix[LD.is_observed,'ROI'] = LD.ix[LD.is_observed,'NAR'] * 100
-LD.ix[~LD.is_observed,'ROI'] = LD.ix[~LD.is_observed,'exp_NAR'] * 100
-
-LD.ix[LD.is_observed,'weighted_ROI'] = LD.ix[LD.is_observed,'weighted_NAR'] * 100
-LD.ix[~LD.is_observed,'weighted_ROI'] = LD.ix[~LD.is_observed,'weighted_exp_NAR'] * 100
-
-LD.ix[LD.is_observed,'mnthly_ROI'] = LD.ix[LD.is_observed,'MAR']
-LD.ix[~LD.is_observed,'mnthly_ROI'] = LD.ix[~LD.is_observed,'exp_MAR']
-
-
-#%%
-stat_counts_by_grade = LD.groupby(['grade','loan_status']).agg({'grade':'count'}) #cuisine-conditional violations counts
-tot_by_grade = LD.groupby('grade').agg({'grade':'count'}) #total violations by cuisine
-grade_cond_stat_dist = stat_counts_by_grade.div(tot_by_grade,level='grade') #cuisine-conditional violation dist
-grade_cond_stat_dist.rename(columns={'grade':'num'},inplace=True)
-grade_cond_stat_dist = grade_cond_stat_dist.reset_index()
-
-tot_by_status = LD.groupby('loan_status').agg({'loan_status':'count'}) #total violations by cuisine
-tot_by_status = tot_by_status/1000 #convert counts to thousands
-tot_by_status.rename(columns={'loan_status':'num'},inplace=True)
-tot_by_status = tot_by_status.reset_index()
 
 #%%
 save_columns = ['ROI',
@@ -147,13 +121,14 @@ save_columns = ['ROI',
  'longitude',
  'loan_amnt',
  'loan_status',
- 'mnthly_ROI',
  'mths_since_last_delinq',
  'mths_since_last_major_derog',
  'mths_since_last_record',
+ 'net_returns',
  'num_add_desc',
  'num_pymnts',
  'open_acc',
+ 'prnc_weight',
  'pub_rec',
  'purpose',
  'revol_bal',
@@ -164,7 +139,6 @@ save_columns = ['ROI',
  'tot_coll_amt',
  'tot_cur_bal',
  'verification_status',
- 'weighted_ROI',
  'zip3']
  
 csv_name = 'all_loans_proc'